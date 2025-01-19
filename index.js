c4aa6751ccb9a7a601fe0f01afd0be795d45033b
Дмитрий Тракало <green_worker@rambler.ru>
2025-01-19 19:28:36 +0300

    add start pack

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hi")
+// fetch("http://localhost:3000/posts/1", {
+//     method: "PUT",
+//     body: JSON.stringify({ "id": 1, "title": "Hello NewWor", "author": "typicode2" })
+// })
+
+fetch("http://localhost:3000/water")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+
+const data = {
+    currentValue: 0,
+    dayValue: 0
+}
+
+const currentValueNode = document.querySelector(".current-value")
+const dayValueNode = document.querySelector(".day-value")
+const buttonAddValueNode = document.querySelector(".increment-value")
+
+function render(data) {
+    currentValueNode.textContent = data.currentValue
+    dayValueNode.textContent = data.dayValue
+}
+
+document.addEventListener("DOMContentLoaded", render(data))
